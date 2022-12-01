da2b7bd382265949c79dd10bfbc8bc22540767be
hafrei <h.a.freiburger@gmail.com>
2022-12-01 00:48:06 -0500

    Day one isn't perfect but it works

---
+++ src/one.rs
@@ -0,0 +1,44 @@
+pub fn run(input: String) {
+    let first = part_one(input.clone());
+    println!("{}", first);
+    let second = part_two(input);
+    println!("{}", second);
+}
+
+fn part_one(input: String) -> u32 {
+    let mut max: u32 = 0;
+    let mut cache: u32 = 0;
+
+    for i in input.lines() {
+        //TODO: if the end of the file isn't a /n, don't drop the last part of the input
+        if i.len() == 0 {
+            if max < cache {
+                max = cache;
+            }
+            cache = 0;
+        } else {
+            cache += i.parse::<u32>().unwrap();
+        }
+    }
+   max 
+}
+
+fn part_two(input: String) -> u32 {
+    let mut sums = Vec::new();
+    let mut cache = 0;
+
+    for i in input.lines() {
+        //TODO: if the end of the file isn't a /n, don't drop the last part of the input
+        if i.len() == 0 {
+            sums.push(cache);
+            cache = 0;
+        } else {
+            cache += i.parse::<u32>().unwrap();
+        }
+    }
+
+    sums.sort();
+    sums.reverse();
+    sums.truncate(3);
+    sums.into_iter().sum()
+}
