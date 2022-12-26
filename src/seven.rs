b393e1155b53ae971b2dd5eccd19a38a01e295f3
hafrei <h.a.freiburger@gmail.com>
2022-12-26 16:41:49 -0500

    Refactor solution to keep track of directory parents

--- src/seven.rs
+++ src/seven.rs
@@ -1,7 +1,8 @@
-use rayon::prelude::*;
 use std::fmt;
 
 const FIRST_PART_MINIMUM: u32 = 100_000;
+const SECOND_PART_MINIMUM: u32 = 30_000_000;
+const DISK_SPACE: u32 = 70_000_000;
 
 #[derive(PartialEq, Eq)]
 enum Commands {
@@ -19,11 +20,33 @@ impl fmt::Display for Commands {
 }
 
 pub fn run(input: String) {
-    let first = first(input);
+    let mut all_dirs = get_dirs(input);
+    let first: u32 = all_dirs
+        .iter()
+        .cloned()
+        .filter(|x| x.size <= FIRST_PART_MINIMUM)
+        .map(|x| x.size)
+        .sum();
     println!("First: {first}");
+
+    all_dirs.sort();
+
+    let root_dir = all_dirs.iter().find(|x| x.name == "/").unwrap(); //please panic if root dir is gone
+    let free_space = DISK_SPACE - root_dir.size;
+    println!("Free space is {free_space}");
+    println!("{all_dirs:#?}");
+    let mut second_candidates: Vec<u32> = all_dirs
+        .iter()
+        .map(|x| x.size)
+        .filter(|x| x >= &SECOND_PART_MINIMUM)
+        .collect();
+    second_candidates.sort();
+    println!("{second_candidates:?}");
+    let second = second_candidates.first();
+    println!("Second: {second:?}");
 }
 
-fn first(input: String) -> u32 {
+fn get_dirs(input: String) -> Vec<ImmidiateDirectory> {
     let new_inter = input
         .split("$")
         .map(|x| {
@@ -35,63 +58,67 @@ fn first(input: String) -> u32 {
         })
         .filter(|x| !x.is_empty())
         .collect::<Vec<Vec<String>>>();
-    let supposed_directory: Vec<Vec<FileType>> =
-        new_inter.par_iter().rev().map(create_structure).collect();
-    let dirs = get_dir_sizes(&supposed_directory);
-    // println!("{dirs:?}");
-    dirs.into_iter()
-        .filter(|x| x.size <= FIRST_PART_MINIMUM)
-        .map(|x| x.size)
-        .sum()
+    let mut parent = "/".to_string();
+    let supposed_directory: Vec<Vec<FileType>> = new_inter
+        .iter()
+        .map(|x| create_structure(x, &mut parent))
+        .filter(|x| !x.is_empty())
+        .collect();
+    get_dir_sizes(&supposed_directory)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct ImmidiateDirectory {
-    name: String,
     size: u32,
+    name: String,
 }
 
 #[derive(Debug)]
 enum FileType {
-    Within(String),
-    Directory(String),
-    NormalFile((String, u32)),
+    Directory((String, String)),       //name, parent
+    NormalFile((String, String, u32)), //name, parent, size
 }
 
 fn get_dir_sizes(supposed_directory: &[Vec<FileType>]) -> Vec<ImmidiateDirectory> {
     let mut dirs: Vec<ImmidiateDirectory> = Vec::new();
+    let mut temp_parent = "/".to_string();
     let mut size: u32 = 0;
 
-    for hopefully_directory in supposed_directory.iter() {
-        if hopefully_directory.is_empty() {
-            continue;
-        } else {
-            for file_descriptor in hopefully_directory.iter() {
-                match file_descriptor {
-                    FileType::NormalFile((_, val)) => size += val,
-                    FileType::Within(name) => {
-                        let new_dir = ImmidiateDirectory {
-                            name: name.to_string(),
-                            size,
-                        };
-                        dirs.push(new_dir);
-                        size = 0;
-                    }
-                    FileType::Directory(name) => {
-                        if let Some(inner_size) =
-                            dirs.iter().find(|a| a.name.eq(name)).map(|x| x.size)
-                        {
-                            size += inner_size;
-                        }
-                    }
-                }
+    for file_descriptor in supposed_directory.iter() {
+        for info in file_descriptor {
+            if let FileType::NormalFile((_, parent, val)) = info {
+                temp_parent = parent.to_string();
+                size += val;
+            };
+        }
+        let dir = ImmidiateDirectory {
+            size,
+            name: temp_parent.clone(),
+        };
+        size = 0;
+        dirs.push(dir);
+    }
+
+    dirs.sort();
+
+    for file_descriptor in supposed_directory.iter() {
+        for info in file_descriptor {
+            if let FileType::Directory((name, parent)) = info {
+                let new_size = if let Some(current) = dirs.iter().find(|x| &x.name == name) {
+                    current.size
+                } else {
+                    panic!("Oh no what how")
+                };
+                if let Some(parent) = dirs.iter_mut().find(|x| &x.name == parent) {
+                    parent.size += new_size;
+                };
             }
         }
     }
     dirs
 }
 
-fn create_structure(input: &Vec<String>) -> Vec<FileType> {
+fn create_structure(input: &Vec<String>, parent: &mut String) -> Vec<FileType> {
     let mut pile = Vec::new();
     for line in input.iter() {
         let mut inner_line: Vec<String> = line
@@ -107,25 +134,25 @@ fn create_structure(input: &Vec<String>) -> Vec<FileType> {
             if inner_line.is_empty() || inner_line.contains(&"..".to_string()) {
                 break;
             } else {
-                let name = inner_line.pop().expect("Nothing for name");
-                pile.push(FileType::Within(name));
-                break;
+                *parent = inner_line.pop().expect("Nothing for name");
             }
         } else if line.contains(&format!("{}", Commands::ListStructure)) && line.len() == 2 {
-            //noop
+            continue;
         } else {
-            if inner_line.contains(&"dir".to_string()) {
+            if inner_line.contains(&"dir".to_string())
+                && !inner_line.iter().any(|w| w.chars().any(|c| c.is_numeric()))
+            {
                 let _ = inner_line.pop().expect("Dropping dir failed");
                 let name = inner_line.pop().expect("Nothing for name");
-                pile.push(FileType::Directory(name));
+                pile.push(FileType::Directory((name, parent.clone())));
             } else {
-                let beeh = inner_line
+                let size = inner_line
                     .pop()
                     .expect("Was empty")
                     .parse::<u32>()
                     .expect("Not a number");
                 let name = inner_line.pop().expect("Nothing for name");
-                pile.push(FileType::NormalFile((name, beeh)));
+                pile.push(FileType::NormalFile((name, parent.clone(), size)));
             }
         }
     }
