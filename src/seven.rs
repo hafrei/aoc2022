4e509805b2dae3652a9f0e6bb6506d6ed43b4ead
hafrei <h.a.freiburger@gmail.com>
2023-11-26 23:51:28 -0500

    Parents are now being tracked properly but there's still the problem of directories having the same name

--- src/seven.rs
+++ src/seven.rs
@@ -55,8 +55,7 @@ pub fn run(input: String) {
     let first: u32 = all_dirs
         .iter()
         .cloned()
-        .filter(|x| x.file_type == FileType::Directory)
-        .filter(|x| x.size <= FIRST_PART_MINIMUM)
+        .filter(|x| x.file_type == FileType::Directory && x.size <= FIRST_PART_MINIMUM)
         .map(|x| x.size)
         .sum();
     println!("First: {first}");
@@ -68,13 +67,14 @@ pub fn run(input: String) {
 
     let space_remaining = DISK_SPACE - root_size;
     println!("Root size is {root_size}, with remaining: {space_remaining}");
-    let needed = if space_remaining > SECOND_PART_MINIMUM {
-        space_remaining - SECOND_PART_MINIMUM
-    } else {
-        SECOND_PART_MINIMUM - space_remaining
-    };
+    let needed = SECOND_PART_MINIMUM - space_remaining;
+    // let needed = if space_remaining > SECOND_PART_MINIMUM {
+    //     space_remaining - SECOND_PART_MINIMUM
+    // } else {
+    //     SECOND_PART_MINIMUM - space_remaining
+    // };
     println!("Needed: {needed}");
-    let second: u32 = all_dirs
+    let second: u32 = all_dirs.clone()
         .iter_mut()
         .filter(|x| x.file_type == FileType::Directory)
         .filter(|x| x.size >= needed)
@@ -82,6 +82,11 @@ pub fn run(input: String) {
         .min()
         .unwrap_or(0);
     println!("Second: {second}"); //2786160 is too high
+                                  //also womp womp 29011935
+    let bup: Vec<FileDetails> = all_dirs
+        .into_iter()
+        .filter(|x| x.file_type == FileType::Directory /*&& x.size >= needed*/).collect();
+    println!("{bup:#?}");
 }
 
 fn get_dirs(input: String, max_depth: &mut i32) -> Vec<FileDetails> {
@@ -90,54 +95,35 @@ fn get_dirs(input: String, max_depth: &mut i32) -> Vec<FileDetails> {
         .map(|x| {
             x.split('\n')
                 .map(|y| y.trim().to_owned())
-                .rev()
                 .filter(|y| !y.is_empty())
                 .collect::<Vec<String>>()
         })
         .filter(|x| !x.is_empty())
         .collect::<Vec<Vec<String>>>();
-    let mut parent = "/".to_string();
+    let base_dir = "".to_string();
+    let mut parent = Vec::new();
+    parent.push(base_dir);
     let mut depth: i32 = 0;
-    let supposed_directory: Vec<FileDetails> = new_inter
+    let mut supposed_directory: Vec<FileDetails> = new_inter
         .iter()
         .map(|x| create_structure(x, &mut parent, &mut depth, max_depth))
         .filter(|x| !x.is_empty())
         .flatten()
         .collect();
-    let supposed_directory = get_dir_sizes(&supposed_directory, *max_depth, FileType::NormalFile);
-    get_dir_sizes(&supposed_directory, *max_depth, FileType::Directory)
-}
-
-fn get_dir_sizes(
-    supposed_directory: &Vec<FileDetails>,
-    max_depth: i32,
-    filter: FileType,
-) -> Vec<FileDetails> {
-    let mut new_dirs: Vec<FileDetails> = supposed_directory.iter().cloned().collect();
-    let mut dir_cache: Vec<FileDetails> = supposed_directory.iter().cloned().collect();
-
-    for i in (0..=max_depth).rev() {
-        for info in dir_cache
-            .iter()
-            .filter(|x| x.depth == i && x.file_type == filter)
-        {
-            if let Some(parent) = new_dirs.iter_mut().find(|x| x.name == info.parent) {
-                parent.size += info.size;
-            }
-        }
-        dir_cache = new_dirs.clone();
-    }
-    new_dirs
+    get_dir_sizes(&mut supposed_directory, *max_depth, FileType::NormalFile);
+    get_dir_sizes(&mut supposed_directory, *max_depth, FileType::Directory);
+    supposed_directory
 }
 
 fn create_structure(
     input: &Vec<String>,
-    parent: &mut String,
+    parent: &mut Vec<String>,
     depth: &mut i32,
     max_depth: &mut i32,
 ) -> Vec<FileDetails> {
-    let mut pile = Vec::new();
-    for line in input.iter() {
+    let mut pile: Vec<FileDetails> = Vec::new();
+    for (e, line) in input.iter().enumerate() {
+        println!("Line {e} is {line:?}");
         let mut inner_line: Vec<String> = line
             .clone()
             .split_whitespace()
@@ -146,9 +132,15 @@ fn create_structure(
             .collect();
 
         if line.contains(&format!("{}", Commands::ParentDirectory)) {
+            println!("   so reduce depth by one");
             *depth -= 1;
+            println!("        and parent was {:?}", parent.last());
+            parent.pop();
+            println!("        and update parent to {:?}", parent.last());
         } else if line.contains(&format!("{}", Commands::ChangeDirectory)) {
+            println!("   so we're changing directory");
             if line.contains('/') {
+                println!("        and adding root");
                 pile.push(FileDetails {
                     name: "/".into(),
                     parent: "/.".into(),
@@ -158,43 +150,79 @@ fn create_structure(
                 });
             }
 
+            println!("   going to increase depth");
             *depth += 1;
             if *depth >= *max_depth {
+                println!("        and update max depth to {}", *depth);
                 *max_depth = *depth;
             }
 
             let _ = inner_line.pop(); //Drop the "cd"
-            *parent = inner_line.pop().expect("Nothing for name");
+            parent.push(inner_line.pop().expect("Nothing for name"));
+            println!("        and update parent to {:?}", parent.last());
         } else if line.contains(&format!("{}", Commands::ListStructure)) && line.len() == 2 {
+            println!("   so treat this as a noop");
             continue;
         } else if inner_line.contains(&"dir".to_string())
             && !inner_line.iter().any(|w| w.chars().any(|c| c.is_numeric()))
-        //Some files have "dir" in the name, but no dir name contains numbers
         {
+            println!("   so we're adding a directory");
             let _ = inner_line.pop().expect("Dropping dir failed");
             let name = inner_line.pop().expect("Nothing for name");
-            pile.push(FileDetails::new(
-                name,
-                parent.clone(),
-                0,
-                *depth,
-                FileType::Directory,
-            ));
+            println!("        and the name is {name}");
+            if let Some(rent) = parent.last() {
+                pile.push(FileDetails::new(
+                    name,
+                    rent.to_owned(),
+                    0,
+                    *depth,
+                    FileType::Directory,
+                ));
+            };
         } else {
+            println!("   so we're adding a file");
             let size = inner_line
                 .pop()
                 .expect("Was empty")
                 .parse::<u32>()
                 .expect("Not a number");
             let name = inner_line.pop().expect("Nothing for name");
-            pile.push(FileDetails::new(
-                name,
-                parent.clone(),
-                size,
-                *depth,
-                FileType::NormalFile,
-            ));
+            println!("        and the name is {name}");
+            if let Some(rent) = parent.last() {
+                pile.push(FileDetails::new(
+                    name,
+                    rent.to_owned(),
+                    size,
+                    *depth,
+                    FileType::NormalFile,
+                ));
+            };
         }
     }
     pile
+}
+
+fn get_dir_sizes(
+    directory: &mut Vec<FileDetails>,
+    max_depth: i32,
+    filter: FileType) {
+
+    for i in (0..=max_depth).rev() {
+    let dir_cache: Vec<FileDetails> = directory.iter().cloned().collect();
+        println!("{i}");
+        for info in dir_cache
+            .iter()
+            .filter(|x| x.depth == i && x.file_type == filter)
+        {
+            println!("Have {info:?}");
+            if let Some(parent) = directory.iter_mut().find(|x| x.name == info.parent) {
+                println!("     Found {parent:?}");
+                parent.size += info.size;
+                println!("     Now {parent:?}\n");
+            } else {
+
+                    panic!("Didn't find a parent! That's not possible unles it's root");
+            }
+        }
+    }
 }
