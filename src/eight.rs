cd1c6daaaa910579f60b14841cf0b0552e985e53
hafrei <h.a.freiburger@gmail.com>
2023-11-30 23:22:03 -0500

    Replace most of day eight with code that allows visualizations of the forest

--- src/eight.rs
+++ src/eight.rs
@@ -1,26 +1,62 @@
-use std::collections::VecDeque;
+use ansiterm::Colour;
+use core::cmp::Ordering;
+use core::fmt::{self, Display};
 
-#[derive(Debug, Clone, Copy, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy)]
 struct Tree {
     x: u32,
     y: u32,
     height: u32,
+    colour: Colour,
 }
 
 impl PartialEq for Tree {
     fn eq(&self, other: &Self) -> bool {
-        (self.x == other.x) & (self.y == other.y) & (self.height == other.height)
+        // (self.x == other.x) & (self.y == other.y) & (self.height == other.height)
+        self.x == other.x
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Copter {
-    direction: Flightpath,
-    step_by: usize,
-    rotate: usize,
-    range_lower: u32,
-    range_higher: u32,
-    len: usize,
+impl PartialOrd for Tree {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Tree {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.x, &self.y, self.height).cmp(&(other.x, &other.y, other.height))
+    }
+}
+
+impl Display for Tree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.colour.paint(self.height.to_string().as_str()))
+    }
+}
+
+impl Eq for Tree {}
+
+struct Forest<'a, T: 'a> {
+    x_max: u32,
+    y_max: u32,
+    trees: &'a [T],
+}
+
+impl<'a, T: fmt::Display + 'a> fmt::Display for Forest<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut len_counter = 0;
+        for item in self.trees {
+            write!(f, "{}", item)?;
+            if len_counter == self.x_max {
+                write!(f, "\n")?;
+                len_counter = 0;
+            } else {
+                len_counter += 1;
+            }
+        }
+        Ok(())
+    }
 }
 
 //Start at opposite, travel in this direction
@@ -33,169 +69,107 @@ enum Flightpath {
 }
 
 pub fn run(input: String) {
-    // println!("{input}");
-    let (forest, x, y) = get_forest(input);
-    let mut walking: VecDeque<u32> = forest.clone().into();
-    let first_copter = Copter {
-        direction: Flightpath::Down,
-        step_by: x,
-        rotate: 1,
-        range_lower: 0,
-        range_higher: y,
-        len: x,
-    };
-    let second_copter = Copter {
-        direction: Flightpath::Right,
-        step_by: 1,
-        rotate: x,
-        range_lower: 0,
-        range_higher: y,
-        len: x,
-    };
-    let third_copter = Copter {
-        direction: Flightpath::Left,
-        step_by: 1,
-        rotate: x,
-        range_lower: 0,
-        range_higher: y,
-        len: x,
-    };
-    let fourth_copter = Copter {
-        direction: Flightpath::Up,
-        step_by: x,
-        rotate: 1,
-        range_lower: 0,
-        range_higher: y,
-        len: x,
-    };
-    let mut visible = Vec::new();
-    visible.append(&mut find_visible(&mut walking, first_copter));
-    // let mut walking: VecDeque<u32> = forest.clone().into(); //Ooof, this sucks
-    // visible.append(&mut find_visible(&mut walking, second_copter));
-    // let mut walking: VecDeque<u32> = forest.clone().into(); //TODO: rotate back properly
-    // visible.append(&mut find_visible(&mut walking, third_copter));
-    // let mut walking: VecDeque<u32> = forest.clone().into();
-    // visible.append(&mut find_visible(&mut walking, fourth_copter));
-    visible.sort();
-    visible.dedup();
-    println!("{}", visible.len()); //619 is too low, 1907 is too high
-    // for t in visible.iter() { //wouldn't it be useful if there could be a coloured visualization
-    // for this?
-    //     println!("{t:?}");
-    // }
-}
-
-fn find_visible(walking: &mut VecDeque<u32>, copter: Copter) -> Vec<Tree> {
-    let mut total_visible = Vec::new();
-    for ypos in copter.range_lower..copter.range_higher {
-        let mut last_visible = 0;
-
-        if copter.direction == Flightpath::Right || copter.direction == Flightpath::Down {
-        let mut x = 0;
-            for n in walking
-                .make_contiguous()
-                .iter()
-                .step_by(copter.step_by)
-                .take(copter.len)
-            {
-                compare_visible(*n, &mut last_visible, &copter, &mut total_visible, x, ypos, 0u32);
-                    if last_visible == 9 {
-                        break
-                    }
-                x += 1;
-            }
-        } else {
-            let mut x = copter.len.try_into().unwrap();
-            for n in walking
-                .make_contiguous()
-                .iter()
-                .step_by(copter.step_by)
-                .take(copter.len)
-                .rev()
-            {
-                compare_visible(*n, &mut last_visible, &copter, &mut total_visible, x, ypos, 5u32);
-                    if last_visible == 9 {
-                        break
-                    }
-                x -= 1;
-            }
-        }
-        walking.rotate_left(copter.rotate);
+    let mut trees = Vec::new();
+    get_colour_forest(input, &mut trees);
+    let mut max_x = 0;
+    let mut max_y = 0;
+    {
+        let Tree { x, y, .. } = trees.iter().last().unwrap();
+        max_x = *x;
+        max_y = *y;
+        let forest = Forest {
+            x_max: *x,
+            y_max: *y,
+            trees: &trees,
+        };
+        println!("{}", forest);
     }
-    total_visible
+    find_visible(&mut trees, Flightpath::Down, max_x, max_y); //working
+    find_visible(&mut trees, Flightpath::Up, max_x, max_y); //working
+    find_visible(&mut trees, Flightpath::Left, max_x, max_y); //working
+    find_visible(&mut trees, Flightpath::Right, max_x, max_y);
+    {
+        let Tree { x, y, .. } = trees.iter().last().unwrap();
+        let forest = Forest {
+            x_max: *x,
+            y_max: *y,
+            trees: &trees,
+        };
+        println!("{}", forest);
+    }
+    let visible = trees.iter().filter(|x| x.colour == Colour::BrightGreen).count();
+    println!("There are {visible} trees");
 }
 
-fn compare_visible(
-    n: u32,
-    last_visible: &mut u32,
-    copter: &Copter,
-    total_visible: &mut Vec<Tree>,
-    x: u32,
-    y: u32,
-    max_band: u32
-) {
-    // println!("{n} vs {}", *last_visible);
-    if n > *last_visible || (x == max_band) {
-        match copter.direction {
-            Flightpath::Up => {
-                let tree = Tree {
-                    y: x-1,
-                    x: y,
-                    height: n,
-                };
-                // println!("{tree:?}");
-                total_visible.push(tree);
-            }
-            Flightpath::Right => {
-                let tree = Tree {
-                    height: n,
-                    y,
-                    x,
-                };
-                // println!("{tree:?}");
-                total_visible.push(tree);
-            }
-            Flightpath::Left => {
-                let tree = Tree {
-                    height: n,
-                    x: x-1,
-                    y,
-                };
-                // println!("{tree:?}");
-                total_visible.push(tree);
-            }
-            Flightpath::Down => {
-                let tree = Tree {
-                    height: n,
-                    y: x,
-                    x: y,
-                };
-                println!("{tree:?}");
-                total_visible.push(tree);
+fn find_visible(trees: &mut Vec<Tree>, path: Flightpath, max_x: u32, max_y: u32) {
+    if path == Flightpath::Right {
+        for i in 0..=max_x {
+            let mut last_visible = 0;
+            // Right: step_by: 1,   take: len
+            for n in trees.iter_mut().filter(|x| x.y == i) {
+                // println!("{n:?}\t{last_visible}");
+                compare_visible(n, &mut last_visible, n.x, 0);
+                if last_visible == 9 {
+                    break;
+                }
             }
         }
-        *last_visible = n;
+    } else if path == Flightpath::Down {
+        //working
+        for i in 0..=max_y {
+            let mut last_visible = 0;
+            for n in trees.iter_mut().filter(|x| x.x == i) {
+                // println!("{n:?}\t{last_visible}");
+                compare_visible(n, &mut last_visible, n.y, 0);
+                if last_visible == 9 {
+                    break;
+                }
+            }
+        }
+    } else if path == Flightpath::Up {
+        for i in 0..=max_y {
+            let mut last_visible = 0;
+            // Up:    step_by: len, take: len
+            for n in trees.iter_mut().filter(|x| x.x == i).rev() {
+                // println!("{n:?}\t{last_visible}");
+                compare_visible(n, &mut last_visible, n.y, max_y);
+                if last_visible == 9 {
+                    break;
+                }
+            }
+        }
+    } else {
+        // Left:  step_by: 1,   take: len
+        for i in 0..=max_x {
+            let mut last_visible = 0;
+            for n in trees.iter_mut().filter(|x| x.y == i).rev() {
+                // println!("{n:?}\t{last_visible}");
+                compare_visible(n, &mut last_visible, n.x, max_x);
+                if last_visible == 9 {
+                    break;
+                }
+            }
+        }
     }
 }
 
-fn get_forest(input: String) -> (Vec<u32>, usize, u32) {
-    let mut x = 0;
-    let forest: Vec<u32> = input
-        .split('\n')
-        .map(|l| {
-            if x == 0 {
-                x = l.len();
-            }
-            l.chars()
-                .map(|x| x.to_digit(10))
-                .filter(|x| x.is_some())
-                .map(|x| x.unwrap())
-                .collect::<Vec<u32>>()
-        })
-        .flatten()
-        .collect();
+fn compare_visible(n: &mut Tree, last_visible: &mut u32, facing_pos: u32, max_pos: u32) {
+    if n.height > *last_visible || facing_pos == max_pos {
+        n.colour = Colour::BrightGreen;
+        *last_visible = n.height;
+    }
+}
 
-    let y = forest.len() / x;
-
-    (forest, x.try_into().unwrap(), y.try_into().unwrap())
+fn get_colour_forest(input: String, forest: &mut Vec<Tree>) {
+    for (i, y) in input.split('\n').enumerate() {
+        for (j, x) in y.chars().enumerate() {
+            let tree = Tree {
+                x: j as u32,
+                y: i as u32,
+                height: (x.to_digit(10).unwrap() as u32),
+                colour: Colour::DarkGray,
+            };
+            forest.push(tree);
+        }
+    }
 }
